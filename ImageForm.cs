2824ced838fb97c3177f7303e77a07f4c34a8681
direwolf420 <amakejk1@web.de>
2020-07-20 13:08:49 +0200

    initial commit

---
+++ ImageForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tTilePadder
+{
+    public partial class ImageForm : Form
+    {
+        public ImageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ResizeImage()
+        {
+            Image.Size = new Size(Width - 2 * 15, Height - 2 * 15 - CloseButton.Height * 2 - 2 * 5);
+        }
+
+        private void ImageForm_Resize(object sender, EventArgs e)
+        {
+            ResizeImage();
+        }
+
+        private void ImageForm_Load(object sender, EventArgs e)
+        {
+            ResizeImage();
+        }
+    }
+}
